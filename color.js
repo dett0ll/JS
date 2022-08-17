1b1fc2316a46c4eb33761e2d0983f981df7eadb5
navjan <105518065+navjan@users.noreply.github.com>
2022-08-17 23:38:06 +0530

    Update color.js

--- color.js
+++ color.js
@@ -1,7 +1,17 @@
-document.addEventListener('DOMContentLoaded', function(){
-    document.querySelectorAll('button').forEach(function(button){
+//document.addEventListener('DOMContentLoaded', function(){
+   // document.querySelectorAll('button').forEach(function(button){
+      //  button.onclick = function(){
+        //    document.querySelector('#hello').style.color = button.dataset.color;
+       // }
+   // })
+//});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('button').forEach((button) => {
         button.onclick = function(){
             document.querySelector('#hello').style.color = button.dataset.color;
         }
     })
 });
+
