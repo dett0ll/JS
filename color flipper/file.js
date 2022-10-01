794756b7efc1b0bc5ed56ac5dc4848c3bc14cc63
navjan <105518065+navjan@users.noreply.github.com>
2022-10-01 16:52:59 +0530

    Add files via upload

---
+++ color flipper/file.js
@@ -0,0 +1,11 @@
+/* function flipper(color){
+    document.body.style.background = color;
+}; */
+
+/* function flipper(color){
+    document.getElementById('color').style.background = color;
+} */
+
+function flipper (color){
+    document.getElementById('color').style.background = color;
+}
